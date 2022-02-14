bdbbac03ab9ce192448baf6990e55fe7d5e878dd
Artyn <GarethPrichett124115@protonmail.com>
2022-02-14 16:40:39 -0600

    add initial files

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+(
+ (org-mode . ((org-hugo-base-dir . "/home/jane/projects/artyns-garden.vercel.app/")
+	      (org-hugo-section . ""))
+ )
+ )
+
